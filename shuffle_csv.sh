910a84e0f489bd2cca8a4b3a2e6159fa7a948950
Juan Lagostena <jlagostena@bitsense.com.ar>
2016-06-06 20:47:42 -0300

    El shuffled reemplaza al original

--- shuffle_csv.sh
+++ shuffle_csv.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-rm -rf "shuffled_$1"
+SHUFFLED_FILE="shuffled_$1"
+
+rm -rf $SHUFFLED_FILE
 
 python -c "import random, sys; lines = open(sys.argv[1]).readlines(); random.shuffle(lines); print ''.join(lines)," $1 > "shuffled_$1"
+
+rm -rf $1
+mv $SHUFFLED_FILE $1
+
+
